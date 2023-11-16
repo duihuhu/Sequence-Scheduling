340a3bf22602c41a99dc54894dce96da01fc907c
duihuhu <duihuhu@163.com>
2023-11-16 13:44:33 +0800

    debug sequence scheduling

--- src/train_lora.py
+++ src/train_lora.py
@@ -102,6 +102,7 @@ def train():
     model = transformers.AutoModelForCausalLM.from_pretrained(
         model_args.model_name_or_path,
         cache_dir=training_args.cache_dir,
+        device_map = "auto",
         load_in_8bit=True,
         torch_dtype=torch.float16,
     )
